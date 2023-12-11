8bcbcdcdfba5256846d3fc97293e52d031da63a1
Trilithus <trilithus@hotmail.com>
2023-12-11 13:08:29 +0100

    Only output session time in s

--- logger_service/udp.go
+++ logger_service/udp.go
@@ -59,7 +59,7 @@ func handleUDPLog(pc net.PacketConn, addr net.Addr, buf []byte) {
 	timestampS := timestampMS / 1000.0
 
 	// Perform any necessary processing on the request body here.
-	file.WriteString(fmt.Sprintf("%5.3f,%10f,%v,%v,%v,%v,%v,%v\r\n", timestampS, timestampMS,
+	file.WriteString(fmt.Sprintf("%5.3f,%v,%v,%v,%v,%v,%v\r\n", timestampS,
 		d["param1"], d["param2"], d["param3"],
 		d["param4"], d["param5"], d["param6"]))
 }
